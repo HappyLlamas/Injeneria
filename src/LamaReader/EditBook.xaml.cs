6f91a9cd558558d3974cf16506fc745dc47a664c
Shevchenko Iryna <90066496+shevchenkoira@users.noreply.github.com>
2022-11-14 16:30:51 +0200

    add modal windows

---
+++ src/LamaReader/EditBook.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LamaReader.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddBook.xaml
+    /// </summary>
+    public partial class EditBook : Window
+    {
+        public EditBook() => InitializeComponent();
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
